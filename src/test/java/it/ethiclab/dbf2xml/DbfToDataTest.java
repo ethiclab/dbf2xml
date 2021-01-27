704a41613ac39a0fe5c3a765a855cf2ce93af34b
Montoya Edu <montoya.edu@gmail.com>
2021-01-27 13:14:34 +0100

    Fix test for Windows and Italian locale.

--- src/test/java/it/ethiclab/dbf2xml/DbfToDataTest.java
+++ src/test/java/it/ethiclab/dbf2xml/DbfToDataTest.java
@@ -3,14 +3,28 @@ package it.ethiclab.dbf2xml;
 import java.io.*;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.*;
+import java.util.Locale;
 
 public class DbfToDataTest {
 
+    static final String OS_NAME = System.getProperty("os.name");
+    static final String LOCALE = Locale.getDefault().toString();
+
     @Test
     public void testFileDoesNotExist() {
+        String expectedMessage = "No such file or directory";
+        if (LOCALE.compareTo("it_IT") == 0) {
+            expectedMessage = "Impossibile trovare il file specificato";
+        }
+        if (OS_NAME.startsWith("Windows")) {
+            assertThatThrownBy(() -> new DbfToDataReader().read("src/test/resources/doesnotexist.dbf"))
+            .isInstanceOf(RuntimeException.class)
+            .hasCause(new FileNotFoundException("src\\test\\resources\\doesnotexist.dbf (" + expectedMessage + ")"));
+            return;
+        }
         assertThatThrownBy(() -> new DbfToDataReader().read("src/test/resources/doesnotexist.dbf"))
         .isInstanceOf(RuntimeException.class)
-        .hasCause(new FileNotFoundException("src/test/resources/doesnotexist.dbf (No such file or directory)"));
+        .hasCause(new FileNotFoundException("src/test/resources/doesnotexist.dbf (" + expectedMessage + ")"));
     }
 
     @Test
